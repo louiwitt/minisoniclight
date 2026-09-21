2e82903fe900de7228853bd7d60d283091057c6d
louiwitt <louisew01@laposte.net>
2026-09-21 20:21:56 +0200

    [ADD] create table drawings

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,11 +4,27 @@ const db = new Database('minisoniclight.db')
 
 db.pragma('foreign_keys = ON')
 
+// Create table users
 db.exec(`
   CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     role TEXT NOT NULL DEFAULT 'user'
+  )
+`)
+
+// Create table drawings. If user is deleted, drawings are automatically deleted too
+db.exec(`
+  CREATE TABLE IF NOT EXISTS drawings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    data TEXT NOT NULL,
+    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id)
+      REFERENCES users(id)
+      ON DELETE CASCADE
   )
 `)
 
