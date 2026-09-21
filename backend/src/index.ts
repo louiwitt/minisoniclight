08dd5b493ec377e4392c8eda1eb3a12b76b3c8c0
louiwitt <louisew01@laposte.net>
2026-09-21 20:01:06 +0200

    [ADD] add login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,52 @@
 import express from 'express'
 import db from './db.js'
 
-console.log(db, 'Index initialized')
-
 const app = express()
 const port = 3000
 
 app.use(express.json())
 
-app.get('/', (_req, res) => {
+// Check API health
+app.get('/health', (_req, res) => {
   res.json({ status: 'ok' })
 })
+
+// Login
+app.post('/login', (req, res) => {
+  const { username } = req.body
+
+  // User cannot login without a username
+  if (typeof username !== 'string' || username.trim() === '') {
+    return res.status(400).json({
+      error: 'Username is required',
+    })
+  }
+
+  // Delete excess spaces
+  const trimmedUsername = username.trim()
+
+  // Check if user already exists
+  const existingUserStmt = db.prepare('SELECT id, username, role FROM users WHERE username = ?')
+  const existingUser = existingUserStmt.get(trimmedUsername)
+
+  // If user exists, return user
+  if (existingUser) {
+    return res.json(existingUser)
+  }
+
+  // Else create user into db
+  const insertUserStmt = db.prepare('INSERT INTO users (username) VALUES (?)')
+  const insertUser = insertUserStmt.run(trimmedUsername)
+
+  return res.status(201).json({
+    id: insertUser.lastInsertRowid,
+    username: trimmedUsername,
+    role: 'user',
+  })
+
+})
+
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
