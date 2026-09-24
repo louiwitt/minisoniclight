b032634d9fed3b0e0546fabc78b6f00a917c8fb3
louiwitt <louisew01@laposte.net>
2026-09-24 22:15:57 +0200

    [ADD] create, get and modify a drawing routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -47,6 +47,123 @@ app.post('/login', (req, res) => {
 })
 
 
+// Create and save a drawing
+app.post('/drawing', (req, res) => {
+  const userId = Number(req.header('X-User-Id'))
+  const { data } = req.body
+
+  // Check if id exists and is an integer
+  if (!Number.isInteger(userId)) {
+    return res.status(401).json({
+      error: 'User identification is required',
+    })
+  }
+
+  // Check the drawing data to save
+  if (typeof data !== 'string' || data.trim() === '') {
+    return res.status(400).json({
+      error: 'Drawing data is required',
+    })
+  }
+
+  // Get user
+  const user = db
+    .prepare('SELECT id FROM users WHERE id = ?')
+    .get(userId)
+
+  if (!user) {
+    return res.status(401).json({
+      error: 'User not found',
+    })
+  }
+
+  // Save drawing
+  const insertDrawingStmt = db
+    .prepare(`
+      INSERT INTO drawings (user_id, data)
+      VALUES (?, ?)
+    `)
+
+  const insertDrawing = insertDrawingStmt.run(userId, data)
+
+  return res.status(201).json({
+    id: insertDrawing.lastInsertRowid,
+    userId,
+    data,
+  })
+
+})
+
+// Get my drawing
+app.get('/drawing', (req, res) => {
+  const userId = Number(req.header('X-User-Id'))
+
+  // Check if id exists and is an integer
+  if (!Number.isInteger(userId)) {
+    return res.status(401).json({
+      error: 'User identification is required',
+    })
+  }
+
+  // Get drawing
+  const drawing = db
+    .prepare(`
+      SELECT id, user_id, data, created_at, updated_at
+      FROM drawings
+      WHERE user_id = ?
+    `)
+    .get(userId)
+
+  if (!drawing) {
+    return res.status(404).json({
+      error: 'Drawing not found',
+    })
+  }
+
+  return res.json(drawing)
+})
+
+
+// Modify my drawing
+app.put('/drawing', (req, res) => {
+  const userId = Number(req.header('X-User-Id'))
+  const { data } = req.body
+
+  // Check if id exists and is an integer
+  if (!Number.isInteger(userId)) {
+    return res.status(401).json({
+      error: 'User identification is required',
+    })
+  }
+
+  // Check the drawing data to save
+  if (typeof data !== 'string' || data.trim() === '') {
+    return res.status(400).json({
+      error: 'Drawing data is required',
+    })
+  }
+
+  // Update
+  const result = db
+    .prepare(`
+      UPDATE drawings
+      SET data = ?, updated_at = CURRENT_TIMESTAMP
+      WHERE user_id = ?
+    `)
+    .run(data, userId)
+
+  // If nothing changed, drawing not found
+  if (result.changes === 0) {
+    return res.status(404).json({
+      error: 'Drawing not found',
+    })
+  }
+
+  return res.json({
+    message: 'Drawing updated',
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
