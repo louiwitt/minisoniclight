65ec24b4d36b864d72b66a3124eaa577729823ba
louiwitt <louisew01@laposte.net>
2026-09-25 16:22:00 +0200

    [ADD] create drawing canvas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import DrawingCanvas from './components/DrawingCanvas.vue'
+
 </script>
 
 <template>
-  <HelloWorld />
+    <main>
+    <h1>MiniSonicLight</h1>
+
+    <DrawingCanvas />
+  </main>
 </template>
---
+++ frontend/src/components/DrawingCanvas.vue
@@ -0,0 +1,36 @@
+<template>
+    <canvas
+        ref="canvas"
+        :width="width"
+        :height="height"
+        style="border: 1px solid white"
+    ></canvas>
+</template>
+
+
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const canvas = ref<HTMLCanvasElement | null>(null)
+const width = 500
+const height = 500
+
+
+onMounted(() => {
+    if (!canvas.value) {
+        return
+    }
+    const context = canvas.value.getContext('2d')
+    if (!context) {
+        return
+    }
+    context.lineCap = 'round'
+    context.lineJoin = 'round'
+    context.lineWidth = 3
+    context.strokeStyle = '#000000'
+})
+
+
+
+</script>
